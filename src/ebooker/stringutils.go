77806d58562f67fb3b3bedbf027925c9eb72a739
Paul Meier <pablo.a.meier@gmail.com>
2012-08-29 14:36:52 -0700

    Stringutils: just StripPrefix and Canonicalize
    I'd like a bit more consistency in our generation, so we'll have a "canonical
    representation" for all prefix tokens. Note that this ISN'T what we actually
    output: it's only useful as a way to key the map, so that "Together" and
    "together" map to the same suffix keys.
    
    The stringutils files provide tools to modify or alter the tweets during the
    input phase, but hopefully also the output phase, if we decide to go that far.

---
+++ src/ebooker/stringutils.go
@@ -0,0 +1,58 @@
+
+package ebooker
+
+import (
+    "regexp"
+    "strings"
+)
+
+
+// Removes the reply-to field from the text of a tweet, e.g. "@SrPablo forget 
+// it!" -> "forget it!" Also removes "public directed tweets"; those tweets that
+// are effectively replies but designed to be seen publicly, such as ".@walmart 
+// what happens when u die?"
+func StripReply(str string) string {
+    replyCheck, _ := regexp.Compile("^(?:\\s*\\.\\s*)?(\\s*@[a-zA-Z0-9_=]+\\s*)")
+    return removePattern(str, replyCheck)
+}
+
+
+// A string is "canonicalized" when it's content has been "neutralized" of what
+// data other than it's core content. We do this on maps to ensure that, for 
+// example, odd capitalizations, or hashtags, or punctuation won't block the 
+// algo from continuing to chain.
+//
+// Note that this is meant to be run not on an entire tweet, but on the tokens
+// that come after splitting into it's non-whitespace components. Also note that
+// we don't "lose" what we strip away -- we put it back in the output phase.
+//
+// A token is in canonical form when:
+//   * It is lowercased.
+//   * All punctuation has been removed.
+func Canonicalize(str string) string {
+    punctuation, _ := regexp.Compile("([\"'\\[\\]().,;:{}|\\\\+=_\\-?<>!@#$%^&*]|\\s)+")
+    stripped := removeAllOfPattern(str, punctuation)
+    return strings.ToLower(stripped)
+}
+
+
+// Removes the first parts of a string that match the pattern.
+func removePattern(str string, pattern *regexp.Regexp) string {
+    found := pattern.Find([]byte(str))
+
+    if found != nil {
+        return strings.Replace(str, string(found), "", 1)
+    }
+
+    return str
+}
+
+// removePattern only removes the first instance of a pattern on a string, this
+// function removes all instances.
+func removeAllOfPattern(str string, pattern *regexp.Regexp) string {
+    compare := removePattern(str, pattern)
+    for compare != removePattern(compare, pattern) {
+        compare = removePattern(compare, pattern)
+    }
+    return compare
+}
---
+++ src/ebooker/stringutils_test.go
@@ -0,0 +1,57 @@
+package ebooker
+
+import (
+	"launchpad.net/gocheck"
+)
+
+// hook up gocheck into the gotest runner.
+type StringUtilsSuite struct{}
+
+var _ = gocheck.Suite(&StringUtilsSuite {})
+
+
+func (s StringUtilsSuite) TestStripReply(c *gocheck.C) {
+
+    test1 := "@SrPablo ...really...? tharly...? really?"
+    expected1 := "...really...? tharly...? really?"
+    runStripReply(test1, expected1, c)
+
+    test2 := "@SelfAwareRoomba nuuuuuuuuuuuuuuuuuuuuuu!!"
+    expected2 := "nuuuuuuuuuuuuuuuuuuuuuu!!"
+    runStripReply(test2, expected2, c)
+
+    test3 := ".@AdamFelber so everything as normal, then?"
+    expected3 := "so everything as normal, then?"
+    runStripReply(test3, expected3, c)
+}
+
+func runStripReply(str1 string, str2 string, c *gocheck.C) {
+    strippedStr := StripReply(str1)
+    c.Assert(strippedStr, gocheck.Equals, str2)
+}
+
+
+func (s StringUtilsSuite) TestCanonicalize(c *gocheck.C) {
+    test1 := "...really...?"
+    expected1 := "really"
+    runCanonicalize(test1, expected1, c)
+
+    test2 := "GODDAMMIT!!"
+    expected2 := "goddammit"
+    runCanonicalize(test2, expected2, c)
+
+    test3 := "normal,"
+    expected3 := "normal"
+    runCanonicalize(test3, expected3, c)
+
+    test4 := "then?ever)"
+    expected4 := "thenever"
+    runCanonicalize(test4, expected4, c)
+}
+
+func runCanonicalize(str1 string, str2 string, c *gocheck.C) {
+    strippedStr := Canonicalize(str1)
+    c.Assert(strippedStr, gocheck.Equals, str2)
+}
+
+
