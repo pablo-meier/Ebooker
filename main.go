0df7343e07da4316a5959fa850c7dc681c1b4e1b
Paul Meier <pablo.a.meier@gmail.com>
2012-09-21 08:59:20 -0700

    Support for multiple user inputs on command-line.
    Now I can make a SrLaurelitaJ0RDAN, and it is glorious.

--- main.go
+++ main.go
@@ -10,17 +10,18 @@ import (
     "flag"
     "fmt"
     "math/rand"
+    "strings"
     "time"
 )
 
 func main() {
 
     // Parse flags, initialize data...
-    var username string
+    var userlist string
     var prefixLen, numTweets int
     var silent, debug, reps, timestamps bool
 
-    flag.StringVar(&username, "user", "laurelita", "Twitter user to base output off of.")
+    flag.StringVar(&userlist, "users", "laurelita", "Comma-separated list of twitter users to base output off of.")
     flag.IntVar(&prefixLen, "prefixLength", 1, "length of prefix")
     flag.IntVar(&numTweets, "numTweets", 50, "the number of tweets to produce")
     flag.BoolVar(&silent, "silent", true, "Generate only the tweets, without other status information.")
@@ -36,43 +37,47 @@ func main() {
     dh := ebooker.GetDataHandle("./ebooker_tweets.db", &logger)
     defer dh.Cleanup()
 
+    users := strings.Split(userlist, ",")
+    var sourcestrings[]string
 
-    // get tweets from persistent storage
-    logger.StatusWrite("Reading from persistent storage...\n")
-    oldTweets := dh.GetTweetsFromStorage(username)
+    for _, username := range users {
+        // get tweets from persistent storage
+        logger.StatusWrite("Reading from persistent storage for %s...\n", username)
+        oldTweets := dh.GetTweetsFromStorage(username)
 
-    tf := ebooker.GetTweetFetcher(&logger)
+        tf := ebooker.GetTweetFetcher(&logger)
 
-    var newTweets []ebooker.TweetData
-    if len(oldTweets) == 0 {
-        logger.StatusWrite("Found no tweets for %s, doing a deep dive to retrieve their history.\n", username)
-        newTweets = tf.DeepDive(username)
-    } else {
-        logger.StatusWrite("Found %d tweets for %s.\n", len(oldTweets), username)
-        newest := oldTweets[len(oldTweets) - 1]
-        newTweets = tf.GetTimelineFromRequest(username, &newest)
+        var newTweets ebooker.Tweets
+        if len(oldTweets) == 0 {
+            logger.StatusWrite("Found no tweets for %s, doing a deep dive to retrieve their history.\n", username)
+            newTweets = tf.DeepDive(username)
+        } else {
+            logger.StatusWrite("Found %d tweets for %s.\n", len(oldTweets), username)
+            newest := oldTweets[len(oldTweets) - 1]
+            newTweets = tf.GetTimelineFromRequest(username, &newest)
+        }
+
+        // update the persistent storage
+        logger.StatusWrite("Inserting %d new tweets into persistent storage.\n", len(newTweets))
+        dh.InsertFreshTweets(username, newTweets)
+
+		copyFrom(&sourcestrings, &oldTweets)
+		copyFrom(&sourcestrings, &newTweets)
     }
 
-    // update the persistent storage
-    logger.StatusWrite("Inserting %d new tweets into persistent storage.\n", len(newTweets))
-    dh.InsertFreshTweets(username, newTweets)
+	// fetch or create a Generator
+	gen := ebooker.CreateGenerator(prefixLen, 140, &logger)
+	if reps {
+		gen.CanonicalizeSources()
+	}
 
-    // fetch or create a Generator
-    gen := ebooker.CreateGenerator(prefixLen, 140, &logger)
-    if reps {
-        gen.CanonicalizeSources()
-    }
-
-    // Seed the Generator
-    for _, tweet := range oldTweets {
-        gen.AddSeeds(tweet.Text)
-    }
-    for _, tweet := range newTweets {
-        gen.AddSeeds(tweet.Text)
-    }
+	// Seed the Generator
+	for _, str := range sourcestrings {
+		gen.AddSeeds(str)
+	}
 
     // Generate some faux tweets. Print them!
-    logger.StatusWrite("Outputting nonsense tweets for %s:\n", username)
+    logger.StatusWrite("Outputting nonsense tweets for \"%v\":\n", userlist)
     var format string
     if silent {
         format = "%s\n"
@@ -85,3 +90,9 @@ func main() {
     }
 }
 
+
+func copyFrom(dst *[]string, src *ebooker.Tweets) {
+	for _, str := range *src {
+		*dst = append(*dst, str.Text)
+	}
+}
